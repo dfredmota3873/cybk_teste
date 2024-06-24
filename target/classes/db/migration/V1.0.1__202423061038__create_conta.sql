eef362aa3a95300c440728312598f3da2d38611b
Fred Mota <127538239+dfredmota3873@users.noreply.github.com>
2024-06-23 22:33:34 -0300

    Delete target directory
    Delete target directory

--- target/classes/db/migration/V1.0.1__202423061038__create_conta.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE cbyk.conta (           id                 uuid NOT NULL,
-                                    data_vencimento    timestamp NOT NULL ,
-                                    data_pagamento     timestamp NULL,
-                                    valor              numeric(19,2) NOT NULL,
-                                    descricao          varchar(240) NOT NULL,
-                                    situacao           varchar(1) NOT NULL,
-                                    CONSTRAINT conta_pkey PRIMARY KEY (id));
